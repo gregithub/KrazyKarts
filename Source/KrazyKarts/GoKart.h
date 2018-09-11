032196b83c817d32c92b610ff28fcf7bf6c16dcb
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2018-09-11 22:45:07 +0200

    Improving steering and turning backwards.

--- Source/KrazyKarts/GoKart.h
+++ Source/KrazyKarts/GoKart.h
@@ -43,7 +43,7 @@ private:
 	UPROPERTY(EditAnywhere)
 		float MaxDrivingForce = 10000;
 	UPROPERTY(EditAnywhere)
-		float MaxDegreesPerSecond= 90;
+		float MinTurningRadius= 10;
 	UPROPERTY(EditAnywhere)
 		float DragCoefficient= 16;//MaxDrivingForce/TopSpeed(m/s)
 	UPROPERTY(EditAnywhere)
